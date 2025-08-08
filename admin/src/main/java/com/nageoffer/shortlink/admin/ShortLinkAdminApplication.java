38bb954f3c3691ffe19a43be1b26ae409edb49fd
setsuna <16049905+anthonylee06@user.noreply.gitee.com>
2025-08-08 15:26:44 +0800

    Initial commit

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,14 @@
+package com.nageoffer.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 20784
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
