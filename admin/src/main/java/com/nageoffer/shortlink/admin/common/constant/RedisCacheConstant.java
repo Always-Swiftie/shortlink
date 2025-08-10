4f23c55fb76b0654b8ef0857f507ce14a2f65453
setsuna <16049905+anthonylee06@user.noreply.gitee.com>
2025-08-10 10:21:04 +0800

    feature:用户注册(使用Redisson分布式锁限制同一用户名同时注册)

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+/**
+ * 后管 Redis 缓存常量类
+ * @author 20784
+ */
+public class RedisCacheConstant {
+
+    public static final String LOCK_USER_REGISTER_KEY = "shortlink:lock_user_register:";
+}
