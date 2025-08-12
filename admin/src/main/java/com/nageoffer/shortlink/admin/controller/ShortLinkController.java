ca82da961e70bfe7d9bafd6aeda9f5a3210edfe5
setsuna <16049905+anthonylee06@user.noreply.gitee.com>
2025-08-12 12:11:19 +0800

    feature:后管系统remote调用短链接管理平台-短链接创建

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/ShortLinkController.java
@@ -5,19 +5,36 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
 import com.nageoffer.shortlink.admin.remote.dto.ShortLinkRemoteService;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 短链接控制层
+ * 短链接控制层 - 后管系统
  * @author 20784
  */
 @RestController
 @RequiredArgsConstructor
 public class ShortLinkController {
+
+    /**
+     * 创建短链接
+     * @param requestParam 短链接创建参数
+     * @return 短链接创建返回参数
+     */
+    @PostMapping("/api/shortlink/v1/admin/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam){
+        ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+        };
+        return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
 
     /**
      * 分页查询短链接
--- admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/ShortLinkRemoteService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -5,7 +5,9 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 
 import java.util.HashMap;
@@ -17,6 +19,21 @@ import java.util.Map;
  */
 public interface ShortLinkRemoteService {
 
+    /**
+     * 创建短链接 - 后管系统
+     * @param requestParam 短链接创建参数
+     * @return 创建响应
+     */
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam){
+        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/shortlink/v1/create", JSON.toJSONString(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>(){});
+    }
+
+    /**
+     *  分页查询短链接 - 后管系统
+     * @param requestParam 分页查询参数
+     * @return 分页查询响应结果
+     */
     default IPage<ShortLinkPageRespDTO> pageShortlink(ShortLinkPageReqDTO requestParam){
         Map<String,Object> requestMap = new HashMap<>();
         requestMap.put("gid",requestParam.getGid());
